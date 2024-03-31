8fd53895612c02c6b59ec6e262f40884a2e4143f
GAMESGLOBALOPER\jared.pillay <jared.pillay@gamesglobal.com>
2024-03-31 19:01:09 +0200

    Added Model for KhumaloCraft

---
+++ CLDV6211_ST10339829_POE/Models/KhumaloCraft.cs
@@ -0,0 +1,11 @@
+﻿namespace CLDV6211_ST10339829_POE.Models
+{
+    public class KhumaloCraft
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
